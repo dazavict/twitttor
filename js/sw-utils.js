0c2c75860a7189d6b13dce8018aab43cb4c3b34f
Victor <dazavict@gmail.com>
2018-12-16 11:07:27 -0300

    modificaciones

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -17,3 +17,28 @@ function actualizaCache(dynanicCache, req, res) {
     }
 
 }
+
+
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
+            return res.clone();
+
+        });
+
+    } else {
+        return res;
+    }
+
+
+
+}
+
